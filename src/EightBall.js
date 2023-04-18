d389a9936e227490b8c2f56380daf8f5fe0740d9
William Squires <william.squiresii@gmail.com>
2023-04-18 14:06:04 -0500

    added freq counter and reset button

--- src/EightBall.js
+++ src/EightBall.js
@@ -18,30 +18,42 @@ import "./EightBall.css"
 function EightBall({ answers }) {
     const [color, setColor] = useState("black");
     const [msg, setMsg] = useState("Think of a question");
+    const [count] = useState({green: 0, goldenrod: 0, red: 0})
 
 
     //TODO: add docstring, still a function
     function handleClick() {
         const randomId = Math.floor(Math.random() * answers.length);
-        //TODO: create newAnswer variable
-        setColor(answers[randomId].color);
-        setMsg(answers[randomId].msg);
+        const newAnswer = answers[randomId];
+        count[newAnswer.color] += 1;
+        setColor(newAnswer.color);
+        setMsg(newAnswer.msg);
     }
-    
+
+    function handleReset() {
+        setColor("black");
+        setMsg("Think of a question");
+    }
+
     return (
-        <div 
-            onClick={handleClick} 
-            className="EightBall" 
-            style={{backgroundColor: color}}
-        >
-            <div>
-                {msg}
+        <div>
+            <div
+                onClick={handleClick}
+                className="EightBall"
+                style={{ backgroundColor: color }}
+            >
+                <div>
+                    {msg}
+                </div>
             </div>
+            <p>Green:{ count.green}</p>
+            <p>Yellow: {count.goldenrod}</p>
+            <p>Red: {count.red}</p>
+            <button onClick={handleReset}>Reset</button>
         </div>
     );
-  }
-  
-  
-  
-  export default EightBall;
-  
+}
+
+
+
+export default EightBall;
